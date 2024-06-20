902f8ea3db4c8e37cd1dee8e728e93cbeac19284
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2024-06-20 21:52:24 +0100

    ERC20Token Contract

---
+++ contracts/contracts/TokenContract.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+//After search I found it is recommended to use ^0.8.0 version, we can change this anytime later 
+pragma solidity ^0.8.0;
+
+//library provides the basic implementation of an ERC20 token.
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+//library provides a basic access control mechanism, allowing only the contract owner to perform certain actions.
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+//the contract is ownable 
+contract ERC20Token is ERC20, Ownable {
+    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
+        //I will create function to return 18 decimals 
+        _mint(msg.sender, initialSupply * 10 ** decimals());
+    }
+
+    //function to mint (should take address and uint256 param)
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    //function to burn (should take uint256 param)
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    //function to return 18 decimals 
+    function decimals() public view virtual override returns (uint8) {
+        return 18;
+    }
+}

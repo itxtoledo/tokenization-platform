82b3953b38a4743af947148219dfb5f02e6a0fac
Gustavo <gustavo@obex.pw>
2024-06-25 12:39:00 +0100

    changed hardhat config to support viaIR in the future

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -2,7 +2,14 @@ import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.26",
+  solidity: {
+    version: "0.8.26",
+    settings: {
+      // https://hardhat.org/hardhat-runner/docs/reference/solidity-support#support-for-ir-based-codegen
+      viaIR: false, // change to true after hardhat update
+    }
+  },
+  
 };
 
 export default config;

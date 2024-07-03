70c842f237af87a9f0e96ea4531dbe42e435005c
EskandarAtrakchi <isgan151@gmail.com>
2024-07-03 16:32:39 +0100

    tests passed for initialSupply, name, symbol, and 18 decimals

--- contracts/test/MintableERC20.ts
+++ contracts/test/MintableERC20.ts
@@ -27,41 +27,6 @@ describe("PresaleFactory", function () {
     };
   }
 
-  // describe("createPresale", function () {
-  //   it("should create a presale contract successfully and verify presale address does not equal cloned presale address", async function () {
-  //     const { presaleFactory, presale, publicClient, otherAccount } = await loadFixture(deployMintableERC20);
-
-  //     const hash = await presaleFactory.write.createPresale([
-  //       "Example",
-  //       "EXM",
-  //       1000n,
-  //       parseEther("0.01"),
-  //     ]);
-
-  //     await publicClient.waitForTransactionReceipt({ hash });
-      
-  //     const presaleEvents = await presaleFactory.getEvents.PresaleCreated();
-  //     await expect(presaleEvents).to.have.lengthOf(1);
-
-  //     const presaleAddress = presaleEvents[0].args.presale; // Accessing the first argument, which is the presale address
-
-  //     // Verify the owner of the cloned presale contract
-  //     const clonedPresale = await hre.viem.getContractAt(
-  //       "Presale",
-  //       presaleAddress as `0x${string}`,
-  //       {
-  //         client: { wallet: otherAccount },
-  //       }
-  //     );
-
-  //     const presaleContract = await presale.address.toString();
-
-  //     const presaleOwner = await clonedPresale.address.toString();
-
-  //     await expect(presaleOwner.toLowerCase()).to.not.equal(presaleContract.toLowerCase());
-  //   });
-  // });
-
   describe("MintableERC20 Initialization", function () {
     it("should have 0n for the initial supply at presale creation", async function () {
       const { owner, presaleFactory, publicClient, token } = await loadFixture(deployMintableERC20);
@@ -193,16 +158,4 @@ describe("PresaleFactory", function () {
       await expect(symbol).to.equal("");
     });
   });
-
-  // describe("Initialization", function () {
-  //   it("Should have 0 balance", async function () {
-  //     const { token, owner, presale } = await loadFixture(deployMintableERC20);
-
-  //     const ownerAddress = presale.address.toString();
-
-  //     const balance = await token.read.balanceOf(ownerAddress);
-
-  //     await expect(balance).to.equal(0);
-  //   });
-  // });
-  });
+});

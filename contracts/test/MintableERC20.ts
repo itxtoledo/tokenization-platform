95e69fefa423cfef96e2d385d3079dac6441e64e
EskandarAtrakchi <isgan151@gmail.com>
2024-07-03 16:08:13 +0100

    names changed

--- contracts/test/MintableERC20.ts
+++ contracts/test/MintableERC20.ts
@@ -62,49 +62,43 @@ describe("PresaleFactory", function () {
   //   });
   // });
 
-  // describe("MintableERC20", function () {
-  //   it("Should assign the initial supply to the owner", async function () {
-  //     const { owner, presaleFactory, publicClient, token } = await loadFixture(deployMintableERC20);
+// describe("MintableERC20", function () {
+//   it("Should assign the initial supply to the owner", async function () {
+//     const { owner, presaleFactory, publicClient, token } = await loadFixture(deployMintableERC20);
 
-  //     const hash = await presaleFactory.write.createPresale([
-  //       "Example",
-  //       "EXM",
-  //       1000n,
-  //       parseEther("0.01"),
-  //     ]);
+//     const initialSupply = 1000n;  // Define the initial supply
+//     const hash = await presaleFactory.write.createPresale([
+//       "Example",
+//       "EXM",
+//       initialSupply,
+//       parseEther("0.01"),
+//     ]);
 
-  //     await publicClient.waitForTransactionReceipt({ hash });
-      
-  //     const presaleEvents = await presaleFactory.getEvents.PresaleCreated();
-  //     await expect(presaleEvents).to.have.lengthOf(1);
+//     await publicClient.waitForTransactionReceipt({ hash });
+    
+//     const presaleEvents = await presaleFactory.getEvents.PresaleCreated();
+//     await expect(presaleEvents).to.have.lengthOf(1);
 
-  //     const factoryAddress = presaleEvents[0].args.presale; // Accessing the first argument, which is the presale address
+//     const presaleAddress = presaleEvents[0].args.presale; // Accessing the first argument, which is the presale address
 
-  //     // Verify the owner of the cloned presale contract
-  //     await hre.viem.getContractAt(
-  //       "PresaleFactory",
-  //       factoryAddress as `0x${string}`,
-  //       {
-  //         client: { wallet: owner },
-  //       }
-  //     );
-      
-  //     console.log('This is to see the owner address ' + owner.account.address + '\nis it the same as 0xA0Cf798816D4b9b9866b5330EEa46a18382f251e')
-      
-  //     const ownerAddressBalance = await publicClient.getBalance({ address: "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266", });
-      
-  //     const checking = await presaleFactory.read.token();
-  //     const name = "Example"
+//     // Verify the owner of the cloned presale contract
+//     await hre.viem.getContractAt(
+//       "PresaleFactory",
+//       presaleAddress as `0x${string}`,
+//       {
+//         client: { wallet: owner },
+//       }
+//     );
+    
+//     const publicClientBalance = await publicClient.
+//     console.log('This is to see the owner address \n ' + owner.account.address + '\nis it the same as 0xA0Cf798816D4b9b9866b5330EEa46a18382f251e');
+    
+//     await expect(publicClientBalance).to.equal(0.000000000000000000);
+//   });
+// });
 
-  //     const deployerAddressBalance = parseEther("0.01 * 10 ** 18") ;
 
-  //     await expect(ownerAddressBalance).to.equal(deployerAddressBalance);
-  //     // await expect(checking).to.equal(name);
-      
-  //   });
-  // });
-
-  describe("PresaleFactory Initialization", function () {
+  describe("MintableERC20 Initialization", function () {
     it("should create a presale with an empty token name", async function () {
       const { token, presaleFactory, owner, publicClient } = await loadFixture(deployMintableERC20);
       const hash = await presaleFactory.write.createPresale([
@@ -136,7 +130,7 @@ describe("PresaleFactory", function () {
     });
   });
 
-  describe("PresaleFactory Initialization", function () {
+  describe("MintableERC20 Initialization", function () {
     it("should create a presale with a token having 18 decimals", async function () {
       const { token, presaleFactory, owner, publicClient } = await loadFixture(deployMintableERC20);
       const hash = await presaleFactory.write.createPresale([
@@ -168,7 +162,7 @@ describe("PresaleFactory", function () {
     });
   });
 
-  describe("PresaleFactory Initialization", function () {
+  describe("MintableERC20 Initialization", function () {
     it("should create a presale with a token having an empty symbol", async function () {
       const { token, presaleFactory, owner, publicClient } = await loadFixture(deployMintableERC20);
       const hash = await presaleFactory.write.createPresale([
